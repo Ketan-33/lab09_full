2361a125ee7ab9e2c782b9fe8a42c1d475b29cce
ketan <ketanpardhi04@gmail.com>
2023-12-23 16:36:15 +0530

    reuseable alert component

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,17 +1,13 @@
 <?php
-
 namespace App\Models;
-
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
     /**
      * The attributes that are mass assignable.
      *
@@ -34,7 +30,6 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
-
     /**
      * The attributes that should be cast.
      *
@@ -44,7 +39,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
-     public function role() {
+
+    public function role() {
         return $this->belongsTo(Role::class);
     }
 }
