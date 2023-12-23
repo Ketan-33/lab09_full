a38a7155c80582390546e405b47645a5572dbf8c
ketan <ketanpardhi04@gmail.com>
2023-12-23 17:28:07 +0530

    Hide user links for authors, add logout link

--- resources/views/layouts/admin/app.blade.php
+++ resources/views/layouts/admin/app.blade.php
@@ -4,13 +4,10 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-
         <title>{{ config('app.name', 'Laravel') }}</title>
-
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
@@ -48,36 +45,29 @@
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
                         >Home</a
                     >
+                    @if(Auth::user()->isAdmin())
                     <a
                         href="{{ route('admin-users') }}"
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
-                        >All Users</a
-                    >
-                    <a
-                          href="{{ route('admin-users-create') }}"
+@@ -58,6 +59,7 @@ class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
                         >Create User</a
                     >
+                    @endif
                     <a
                         href="{{ route('admin-categories') }}"
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
-                        >All Categories</a
-                    >
-                    <a
-                        href="{{ route('admin-categories-create') }}"
-                        class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
-                        >Create Category</a
-                    >
-                    <a
-                        href="#"
-                        class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
-                        >All Posts</a
-                    >
-                    <a
-                        href="#"
+@@ -78,6 +80,13 @@ class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
                         class="block px-4 py-2 hover:bg-indigo-800 rounded-md"
                         >Create Post</a
                     >
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <a :href="route('logout')"
+                            class="block px-4 py-2 hover:bg-indigo-800 rounded-md cursor-pointer"
+                            onclick="event.preventDefault();this.closest('form').submit();">
+                                    {{ __('Log Out') }}</a>
+                    </form>
                 </li>
             </ul>
         </nav>
